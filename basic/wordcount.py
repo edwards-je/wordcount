06bc083e438b66d9b7e454d745b1ce1fd081795d
edwards-je <edwards_jonathon@protonmail.com>
2021-07-03 16:42:31 -0500

    added functions to complete wordcount.py

--- basic/wordcount.py
+++ basic/wordcount.py
@@ -39,16 +39,63 @@ print_words() and print_top().
 
 import sys
 
-# +++your code here+++
-# Define print_words(filename) and print_top(filename) functions.
-# You could write a helper utility function that reads a file
-# and builds and returns a word/count dict for it.
-# Then print_words() and print_top() can just call the utility function.
+# read text file
+def text_read(file):
+    with open(file, 'r') as f:
+        text = f.read()
+    return text
 
-###
+# takes in string as argument and return array of separated words, non-alphanumeric characters removed
+def clean_text(text):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz \''
+    cleantext = ""
+    text = text_read(text).lower()
+    for i in text:
+        if i in alphabet:
+            cleantext += i
+        else:
+            cleantext += ' '
+    cleantext = cleantext.split()
+    cleantext[:] = [x for x in cleantext if x != '\'']
+    
+    return(cleantext)
 
-# This basic command line argument parsing code is provided and
-# calls the print_words() and print_top() functions which you must define.
+# count and add words to dictionary
+def count_append(words, word_count_hash):
+    for i in words:
+        if word_count_hash.has_key(i):
+            word_count_hash[i] += 1
+        else:
+            word_count_hash[i] = 1
+
+    #print(word_count_hash)
+    return(word_count_hash)
+
+
+# create dicitonary from text
+def dict_create(seed):
+    words_array = clean_text(seed)
+    count_dict = {}
+    count_dict = count_append(words_array, count_dict)
+    return(count_dict)
+
+
+# returns sorted count in <word 1, count 1> format: for --count
+def print_words(text):
+    text_dict = dict_create(text  )
+    for key in sorted(text_dict):
+        s = "."
+        print (str(key) + " " +  s*(12 - len(key)) + str(text_dict[key]))
+
+
+# returns 20 words from most used to least
+def print_top(text):
+    text_dict = dict_create(text)
+    sorted_list = sorted(text_dict.items(), key=lambda tup: tup[1], reverse=True)
+    for key in sorted_list[:20]:
+      s = "."
+      print(str(key[0]) + " " + s*(12 - len(key[0])) + str(key[1]))
+
 def main():
   if len(sys.argv) != 3:
     print 'usage: ./wordcount.py {--count | --topcount} file'
@@ -58,11 +105,11 @@ def main():
   option = sys.argv[1]
   filename = sys.argv[2]
   if option == '--count':
-    # print_words(filename)
-    print("Hello from --count")
+    print_words(filename)
+    # print("Hello from --count")
   elif option == '--topcount':
-    print("Hello from --topcount")
-    # print_top(filename)
+    # print("Hello from --topcount")
+    print_top(filename)
   else:
     print 'unknown option: ' + option
     sys.exit(1)
