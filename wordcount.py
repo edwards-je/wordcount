57b1561c76f9809e5f1f6207e33c3e64744243a5
edwards-je <edwards_jonathon@protonmail.com>
2021-07-07 09:29:23 -0500

    cleaned repo for storage

--- wordcount.py
+++ wordcount.py
@@ -6,35 +6,21 @@
 # Google's Python Class
 # http://code.google.com/edu/languages/google-python-class/
 
-"""Wordcount exercise
+# Code for Google's Python Class
+# Copyright 2010 Google Inc.
+
+# main() developed by Nick Parlante
+# at Google Inc.
+
+"""
+A word count program as taught in Google's Python Class. Takes .txt input, cleans the text and removes 
+non-alphanumeric characters, and returns an alphabetized list of word counts (--count), or the top 20
+most common words (--topcount).
+
+Usage: ./wordcount.py {--count | --topcount} <file>
+
+Wordcount exercise
 Google's Python class
-
-The main() below is already defined and complete. It calls print_words()
-and print_top() functions which you write.
-
-1. For the --count flag, implement a print_words(filename) function that counts
-how often each word appears in the text and prints:
-word1 count1
-word2 count2
-...
-
-Print the above list in order sorted by word (python will sort punctuation to
-come before letters -- that's fine). Store all the words as lowercase,
-so 'The' and 'the' count as the same word.
-
-2. For the --topcount flag, implement a print_top(filename) which is similar
-to print_words() but which prints just the top 20 most common words sorted
-so the most common word is first, then the next most common, and so on.
-
-Use str.split() (no arguments) to split on all whitespace.
-
-Workflow: don't build the whole program at once. Get it to an intermediate
-milestone and print your data structure and sys.exit(0).
-When that's working, try for the next milestone.
-
-Optional: define a helper function to avoid code duplication inside
-print_words() and print_top().
-
 """
 
 import sys
